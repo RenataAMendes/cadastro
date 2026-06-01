8d8487d3a0f7466ec723ae2cbaebfcb71477a1b2
Predo <104280947+BrazilianMarket@users.noreply.github.com>
2026-06-01 16:28:40 -0300

    atualização do app.py

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
 
+from models import init_db
 
 app = Flask(__name__)
+
+init_db()
+
 
 @app.route("/")
 def login():
     return render_template("login.html")
+
+
+# @app.route("/home")
+# def home():
+#     return render_template("index.html")
+
+
+# @app.route("/produtos")
+# def produtos():
+#     return render_template("produtos.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
